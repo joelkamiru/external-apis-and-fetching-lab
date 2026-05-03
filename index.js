6fde6b54230ce938903d6a1e235b6229dee3731d
Joel Kamiru <kamirujoel2000@gmail.com>
2026-05-03 09:31:01 +0300

    selected elements in js and test git

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+
+const stateInput = document.getElementById("state-input");
+const fetchButton = document.getElementById("get-alerts");
+const alertDisplay = document.getElementById("alert-display");
+const errorMessage = document.getElementById("error-message");
+const alertList = document.getElementById("alert-list");
+const summaryMessage = document.getElementById("summary-message");
+const spinner = document.getElementById("loading-spinner");
